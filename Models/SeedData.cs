6e96d2001f951570f2036980cd0c9dd1dad70dbf
Marshall <32920499+YungMoose@users.noreply.github.com>
2020-03-07 21:50:17 -0700

    Add project files.

---
+++ Models/SeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Johnny Lingo",
+                        ReleaseDate = DateTime.Parse("1979-1-11"),
+                        Genre = "Self Worth",
+                        Rating = "G",
+                        Price = 7.99M
+                    }
+
+                    
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
